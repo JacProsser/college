ec3112f2da965fdf4d73c59b50cdaa4325ec1b8a
JacProsser <jacleighp@gmail.com>
2021-01-21 13:37:31 +0000

    update using function:  "def speed"

--- 25.py
+++ 25.py
@@ -2,13 +2,17 @@ import colorama
 from colorama import Fore
 import os
 
+def speed(distance, time):
+    output = distance / time
+    return  output
+    
 os.system("cls")
 def process():
     os.system("cls")
     print("Average Speed Calculator in",  Fore.YELLOW+"python.", Fore.RESET+"| Made by", Fore.CYAN+"Jac Prosser\n" + Fore.RESET)
-    distanceM = float(input("Distance (m): "))
-    timeS = float(input("Time (s): "))
-    sum = distanceM / timeS
+    distance = float(input("Distance (M): "))
+    time = float(input("Time (s): "))
+    sum = speed(distance, time)
     
     print(sum, "M/S")
 
