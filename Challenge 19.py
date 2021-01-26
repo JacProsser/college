8a8a5fc51a6952fb821380335197b4d3dbb8c692
JacProsser <jacleighp@gmail.com>
2021-01-26 11:21:17 +0000

    add .lower()

--- Challenge 19.py
+++ Challenge 19.py
@@ -15,12 +15,12 @@ def spice():
        os.system("cls")
        print("Spice Girls [Challenge] in",  Fore.YELLOW+"python.", Fore.RESET+"| Made by", Fore.CYAN+"Jac Prosser\n" + Fore.RESET)
        print("These are the nicknames/stage names for the SPICE GIRLS! | Do you know all of their real names?")
-       print("\nNicknames: Scary Spice, Sporty Spice, Baby Spice, Ginger Spice and Posh Spice")
-       scary_spice = input("\nWhat is Scary Spice's name? ")
-       sporty_spice = input("\nWhat is Sporty Spice's name? ")
-       baby_spice = input("\nWhat is Baby Spice's name? ")
-       ginger_spice = input("\nWhat is Ginger Spice's name? ")
-       posh_spice = input("\nWhat is Posh Spice's name? ")
+       print("Nicknames: Scary Spice, Sporty Spice, Baby Spice, Ginger Spice and Posh Spice")
+       scary_spice = input("\nWhat is Scary Spice's name? ").lower()
+       sporty_spice = input("\nWhat is Sporty Spice's name? ").lower()
+       baby_spice = input("\nWhat is Baby Spice's name? ").lower()
+       ginger_spice = input("\nWhat is Ginger Spice's name? ").lower()
+       posh_spice = input("\nWhat is Posh Spice's name? ").lower()
        
        if scary_spice == realnames[0] and sporty_spice == realnames[1] and baby_spice == realnames[2] and ginger_spice == realnames[3] and posh_spice == realnames[4]:
            print(Fore.GREEN + "\nViva Forever!")
